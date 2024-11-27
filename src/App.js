fd9d826a36c075544960ea874cf147d9b3705b6f
Shahdfekry <103386196+Shahdfekry@users.noreply.github.com>
2024-11-27 23:59:35 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [year, setYear] = useState("");
   const [cvc, setCVC] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [showForm, setShowForm] = useState(true);
   // errors
   const [blankErrors, setBlankErrors] = useState({
     name: "",
@@ -23,7 +24,6 @@ function App() {
   });
   const [nameError, setNameError] = useState("");
   const [yearError, setYearError] = useState("");
-  const [showForm, setShowForm] = useState(true);
 
   /* handlers */
   const handleBlankError = (field, value) => {
@@ -92,6 +92,7 @@ function App() {
 
     setShowForm(false);
   }
+
   function handleReset() {
     setShowForm(true);
     setName("");
@@ -104,48 +105,56 @@ function App() {
 
   return (
     <div className="container">
-      <Card
-        name={name}
-        cardNumber={cardNumber}
-        month={month}
-        year={year}
-        cvc={cvc}
-      />
+      <Card>
+        <CardFront>
+          <Circles />
+          <CardDetails
+            name={name}
+            cardNumber={cardNumber}
+            month={month}
+            year={year}
+          />
+        </CardFront>
+        <CardBack cvc={cvc} />
+      </Card>
       {showForm ? (
-        <Form
-          cardNumber={cardNumber}
-          setCardNumber={(value) => {
-            const formattedValue = formatCardNumber(value);
-            setCardNumber(formattedValue);
-            handleBlankError("cardNumber", value);
-            handleNumberError("cardNumber", value);
-          }}
-          month={month}
-          setMonth={(value) => {
-            setMonth(value);
-            handleBlankError("month", value);
-            handleNumberError("month", value);
-          }}
-          year={year}
-          setYear={(value) => {
-            handleYearError(value);
-            handleBlankError("year", value);
-            handleNumberError("year", value);
-          }}
-          cvc={cvc}
-          setCVC={(value) => {
-            setCVC(value);
-            handleBlankError("cvc", value);
-            handleNumberError("cvc", value);
-          }}
-          blankErrors={blankErrors}
-          numberErrors={numberErrors}
-          inputValue={inputValue}
-          nameError={nameError}
-          yearError={yearError}
-          handleSubmit={handleSubmit}
-          handleNameChange={handleNameChange}
-        />
+        <Form handleSubmit={handleSubmit}>
+          <NameInput
+            blankErrors={blankErrors}
+            nameError={nameError}
+            inputValue={inputValue}
+            handleNameChange={handleNameChange}
+          />
+          <CardNumberInput
+            blankErrors={blankErrors}
+            numberErrors={numberErrors}
+            cardNumber={cardNumber}
+            formatCardNumber={formatCardNumber}
+            setCardNumber={setCardNumber}
+            handleBlankError={handleBlankError}
+            handleNumberError={handleNumberError}
+          />
+          <div className="date-cvc">
+            <DateInput
+              blankErrors={blankErrors}
+              numberErrors={numberErrors}
+              month={month}
+              setMonth={setMonth}
+              handleBlankError={handleBlankError}
+              handleNumberError={handleNumberError}
+              yearError={yearError}
+              handleYearError={handleYearError}
+            />
+            <CVCInput
+              blankErrors={blankErrors}
+              numberErrors={numberErrors}
+              cvc={cvc}
+              setCVC={setCVC}
+              handleBlankError={handleBlankError}
+              handleNumberError={handleNumberError}
+            />
+          </div>
+        </Form>
       ) : (
         <CompletedState handleReset={handleReset} />
       )}
@@ -153,160 +162,209 @@ function App() {
   );
 }
 
-function Card({ name, cardNumber, month, year, cvc }) {
+function Card({ children }) {
+  return <div className="card">{children}</div>;
+}
+function CardFront({ children }) {
   return (
-    <div className="card">
-      <div className="card-back">
-        <img src="/images/bg-card-back.png" alt="card back" />
-        <div className="CVC">
-          <p>{cvc ? cvc : "000"}</p>
-        </div>
+    <div className="card-front">
+      <img src="/images/bg-card-front.png" alt="card front" />
+      {children}
+    </div>
+  );
+}
+function Circles() {
+  return (
+    <div className="circles">
+      <div className="white-circle"></div>
+      <div className="transparent-circle"></div>
+    </div>
+  );
+}
+function CardDetails({ name, cardNumber, month, year }) {
+  return (
+    <div className="card-details">
+      <div class="card-number">
+        {cardNumber ? cardNumber : <p>0000 0000 0000 0000</p>}
       </div>
-      <div className="card-front">
-        <img src="/images/bg-card-front.png" alt="card front" />
-        <div className="circles">
-          <div className="white-circle"></div>
-          <div className="transparent-circle"></div>
-        </div>
-        <div className="card-details">
-          <div class="card-number">
-            {cardNumber ? cardNumber : <p>0000 0000 0000 0000</p>}
-          </div>
-          <div className="row">
-            <p class="card-name">{name ? name : "JANE APPLESEED"}</p>
-            <p class="card-expiry">
-              {month ? month : "00"}/{year ? year : "00"}
-            </p>
-          </div>
-        </div>
+      <div className="row">
+        <p class="card-name">{name ? name : "JANE APPLESEED"}</p>
+        <p class="card-expiry">
+          {month ? month : "00"}/{year ? year : "00"}
+        </p>
       </div>
     </div>
   );
 }
-function Form({
-  cardNumber,
-  setCardNumber,
-  month,
-  setMonth,
-  year,
-  setYear,
-  cvc,
-  setCVC,
-  blankErrors,
-  numberErrors,
-  handleSubmit,
-  inputValue,
-  nameError,
-  handleNameChange,
-  yearError,
-}) {
+function CardBack({ cvc }) {
+  return (
+    <div className="card-back">
+      <img src="/images/bg-card-back.png" alt="card back" />
+      <div className="CVC">
+        <p>{cvc ? cvc : "000"}</p>
+      </div>
+    </div>
+  );
+}
+function Form({ children, handleSubmit }) {
   return (
     <form onSubmit={handleSubmit}>
       <div className="form-info">
-        <div className="name">
-          <label>CARDHOLDER NAME</label>
-          <input
-            className={blankErrors.name || nameError ? "error" : ""}
-            type="text"
-            placeholder="e.g. Jane Appleseed"
-            value={inputValue}
-            onChange={handleNameChange}
-          />
-          {blankErrors.name && (
-            <span className="error-message">{blankErrors.name}</span>
-          )}
-          {nameError && <span className="error-message">{nameError}</span>}
-        </div>
-        <div className="card-num">
-          <label>CARD NUMBER</label>
-          <input
-            className={
-              blankErrors.cardNumber || numberErrors.cardNumber ? "error" : ""
-            }
-            type="text"
-            placeholder="e.g. 1234 5678 9123 0000"
-            value={cardNumber}
-            onChange={(e) => {
-              const value = e.target.value;
-              setCardNumber(value.length <= 19 ? value : cardNumber);
-              console.log(value.length);
-            }}
-          />
-          {blankErrors.cardNumber && (
-            <span className="error-message">{blankErrors.cardNumber}</span>
-          )}
-          {numberErrors.cardNumber && (
-            <span className="error-message">{numberErrors.cardNumber}</span>
-          )}
-        </div>
-        <div className="date-cvc">
-          <div className="date">
-            <label>EXP. DATE (MM/YY)</label>
-            <span>
-              <div className="wrapper">
-                <input
-                  className={
-                    blankErrors.month || numberErrors.month ? "error" : ""
-                  }
-                  type="text"
-                  placeholder="MM"
-                  value={month}
-                  onChange={(e) => setMonth(e.target.value)}
-                />
-                {blankErrors.month && (
-                  <span className="error-message">{blankErrors.month}</span>
-                )}
-                {numberErrors.month && (
-                  <span className="error-message">{numberErrors.month}</span>
-                )}
-              </div>
-              <div className="wrapper">
-                <input
-                  className={
-                    blankErrors.year || numberErrors.year || yearError
-                      ? "error"
-                      : ""
-                  }
-                  type="text"
-                  placeholder="YY"
-                  value={year}
-                  onChange={(e) => setYear(e.target.value)}
-                />
-                {blankErrors.year && (
-                  <span className="error-message">{blankErrors.year}</span>
-                )}
-                {numberErrors.year && (
-                  <span className="error-message">{numberErrors.year}</span>
-                )}
-                {yearError && (
-                  <span className="error-message">{yearError}</span>
-                )}
-              </div>
-            </span>
-          </div>
-          <div className="cvc">
-            <label>CVC</label>
-            <input
-              className={blankErrors.cvc || numberErrors.cvc ? "error" : ""}
-              type="text"
-              placeholder="e.g. 123"
-              value={cvc}
-              onChange={(e) => setCVC(e.target.value)}
-            />
-            {blankErrors.cvc && (
-              <span className="error-message">{blankErrors.cvc}</span>
-            )}
-            {numberErrors.cvc && (
-              <span className="error-message">{numberErrors.cvc}</span>
-            )}
-          </div>
-        </div>
+        {children}
         <Button>Confirm</Button>
       </div>
     </form>
   );
 }
+function NameInput({ blankErrors, nameError, inputValue, handleNameChange }) {
+  return (
+    <div className="name">
+      <label>CARDHOLDER NAME</label>
+      <input
+        className={blankErrors.name || nameError ? "error" : ""}
+        type="text"
+        placeholder="e.g. Jane Appleseed"
+        value={inputValue}
+        onChange={handleNameChange}
+      />
+      {blankErrors.name && (
+        <span className="error-message">{blankErrors.name}</span>
+      )}
+      {nameError && <span className="error-message">{nameError}</span>}
+    </div>
+  );
+}
+function CardNumberInput({
+  blankErrors,
+  numberErrors,
+  cardNumber,
+  formatCardNumber,
+  setCardNumber,
+  handleBlankError,
+  handleNumberError,
+}) {
+  return (
+    <div className="card-num">
+      <label>CARD NUMBER</label>
+      <input
+        className={
+          blankErrors.cardNumber || numberErrors.cardNumber ? "error" : ""
+        }
+        type="text"
+        placeholder="e.g. 1234 5678 9123 0000"
+        value={cardNumber}
+        onChange={(e) => {
+          const value = e.target.value;
+          const formattedValue = formatCardNumber(value);
+          setCardNumber(value.length <= 19 ? formattedValue : cardNumber);
+          handleBlankError("cardNumber", value);
+          handleNumberError("cardNumber", value);
+        }}
+      />
+      {blankErrors.cardNumber && (
+        <span className="error-message">{blankErrors.cardNumber}</span>
+      )}
+      {numberErrors.cardNumber && (
+        <span className="error-message">{numberErrors.cardNumber}</span>
+      )}
+    </div>
+  );
+}
+function DateInput({
+  blankErrors,
+  numberErrors,
+  month,
+  setMonth,
+  year,
+  handleBlankError,
+  handleNumberError,
+  yearError,
+  handleYearError,
+}) {
+  return (
+    <div className="date">
+      <label>EXP. DATE (MM/YY)</label>
+      <span>
+        <div className="wrapper">
+          <input
+            className={blankErrors.month || numberErrors.month ? "error" : ""}
+            type="text"
+            placeholder="MM"
+            value={month}
+            onChange={(e) => {
+              const value = e.target.value;
+              setMonth(value);
+              handleBlankError("month", value);
+              handleNumberError("month", value);
+            }}
+          />
+          {blankErrors.month && (
+            <span className="error-message">{blankErrors.month}</span>
+          )}
+          {numberErrors.month && (
+            <span className="error-message">{numberErrors.month}</span>
+          )}
+        </div>
+        <div className="wrapper">
+          <input
+            className={
+              blankErrors.year || numberErrors.year || yearError ? "error" : ""
+            }
+            type="text"
+            placeholder="YY"
+            value={year}
+            onChange={(e) => {
+              const value = e.target.value;
+              handleYearError(value);
+              handleBlankError("year", value);
+              handleNumberError("year", value);
+            }}
+          />
+          {blankErrors.year && (
+            <span className="error-message">{blankErrors.year}</span>
+          )}
+          {numberErrors.year && (
+            <span className="error-message">{numberErrors.year}</span>
+          )}
+          {yearError && <span className="error-message">{yearError}</span>}
+        </div>
+      </span>
+    </div>
+  );
+}
 
+function CVCInput({
+  blankErrors,
+  numberErrors,
+  cvc,
+  setCVC,
+  handleBlankError,
+  handleNumberError,
+}) {
+  return (
+    <div className="cvc">
+      <label>CVC</label>
+      <input
+        className={blankErrors.cvc || numberErrors.cvc ? "error" : ""}
+        type="text"
+        placeholder="e.g. 123"
+        value={cvc}
+        onChange={(e) => {
+          const value = e.target.value;
+          setCVC(value);
+          handleBlankError("cvc", value);
+          handleNumberError("cvc", value);
+        }}
+      />
+      {blankErrors.cvc && (
+        <span className="error-message">{blankErrors.cvc}</span>
+      )}
+      {numberErrors.cvc && (
+        <span className="error-message">{numberErrors.cvc}</span>
+      )}
+    </div>
+  );
+}
 function CompletedState({ handleReset }) {
   return (
     <div className="completed-state">
